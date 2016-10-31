2b46c5a7c96c981b1de5cc3237f93c057c5f2e59
darthfett <darthfett@gmail.com>
2016-10-31 16:35:47 -0700

    Decks and Hands can be drawn

--- bgl/resource.py
+++ bgl/resource.py
@@ -7,7 +7,7 @@ class ResourcePool:
     def draw(self, window):
         raise NotImplementedError('Subclass {c} of ResourcePool must implement draw method.'.format(c=self.__class__.__name__))
 
-class Deck(ResourcePool):
+class CardPool(ResourcePool):
     def draw_card(self):
         if self.cards:
             return self.cards.pop()
@@ -23,3 +23,45 @@ class Deck(ResourcePool):
             cards = []
         
         self.cards = cards
+
+class Deck(CardPool):
+    DECK_THICKNESS = 5
+    def draw(self):
+        if not self.cards: return
+        
+        glPushMatrix()
+        
+        for card in reversed(self.cards[:Deck.DECK_THICKNESS]):
+            card.cardback.draw()
+            
+            glTranslatef(2, 2, 0)
+        
+        glPopMatrix()
+
+class Hand(CardPool):
+    def draw(self, center_offset=0, degrees_between_cards=8.5):
+        if not self.cards: return
+        
+        glPushMatrix()
+        
+        if center_offset:
+            glTranslatef(0, -center_offset, 0)
+        
+        glRotatef(degrees_between_cards * ceil(len(self.cards) / 2), 0, 0, 1)
+        
+        for card in self.cards:
+            
+            glPushMatrix()        
+        
+            if center_offset:
+                glTranslatef(0, center_offset, 0)
+                
+            card.draw()
+            
+            glPopMatrix()
+            
+            glRotatef(-degrees_between_cards, 0, 0, 1)
+            
+        
+        glPopMatrix()
+        
